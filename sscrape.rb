1d399d0ef10a58485c92a1c33ad4dd372f0b1924
cballrun <carter.k.ball@gmail.com>
2024-02-28 16:30:44 -0500

    User able to search teams and return total number of wins

--- sscrape.rb
+++ sscrape.rb
@@ -1,13 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 puts "Enter a search term"
 search_term = gets.chomp 
 
 doc = Nokogiri::HTML5(URI.open("https://www.scrapethissite.com/pages/forms/?q=#{search_term}"))
 
+teams = {}
+
 doc.css("tr.team").each do |team|
-    puts team.at(".name").content.strip 
+    team_name = team.at(".name").content.strip
+    team_wins = team.at(".wins").content.to_i
+
+    teams[team_name] ||= 0
+    teams[team_name] += team_wins
 end
+
+teams.each do |team_name, team_wins|
+    puts "#{team_name} => #{team_wins}"
+end
+
 
 
